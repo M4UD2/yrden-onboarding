27c291810514186c9e152c592d2f024b4cd46386
Lívia Miranda <livia146miranda@gmail.com>
2026-01-14 09:50:23 -0300

    djskl

--- src/components/atoms/Badge/index.tsx
+++ src/components/atoms/Badge/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { cn } from "../../utils";
+import { cn } from "../../../utils";
 
 interface BadgeProps {
   variant?: "default" | "secondary" | "destructive" | "outline" | "success" | "warning" | "info";
